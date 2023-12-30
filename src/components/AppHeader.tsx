dd7fb57af1acd6b1e3fe9cd809277d724b79dda1
Cao Nam <nam52871@donga.edu.vn>
2023-12-30 13:04:15 +0700

    Create: foating contact

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -4,6 +4,7 @@ import AppSearchBar from './AppSearchBar'
 import { FaChevronDown } from 'react-icons/fa6'
 import { Link } from 'react-router-dom'
 import { HiMenu } from 'react-icons/hi'
+import AppTooltip from './AppTooltip'
 
 export default function AppHeader() {
   const listMenu = ['SẢN PHẨM', 'COMBO', 'KHUYẾN MÃI', 'GÓI THÀNH VIÊN', 'GÓC CHIA SẺ', 'VỀ CHÚNG TÔI ']
@@ -43,21 +44,26 @@ export default function AppHeader() {
             </div>
           </div>
           <div className='flex gap-4 ml-14 justify-end'>
-            <Link
-              to=''
-              className='hover:bg-opacity-30 duration-300 transition-all w-10 h-10 flex items-center justify-center rounded-full bg-primary bg-opacity-10'
-            >
-              <FiUser size={20} className='text-greenDark' />
-            </Link>
-            <Link
-              to=''
-              className='hover:bg-opacity-30 duration-300 transition-all relative w-10 h-10 flex items-center justify-center rounded-full bg-primary bg-opacity-10'
-            >
-              <FiShoppingCart size={20} className='text-greenDark' />
-              <div className='rounded-full p-[11px] top-[-10%] right-[-10%] absolute w-3 h-3 text-white text-xs bg-red-600 flex items-center justify-center'>
-                10
-              </div>
-            </Link>
+            <AppTooltip content='Tài khoản'>
+              <Link
+                type='button'
+                to=''
+                className='hover:bg-opacity-30 duration-300 transition-all w-10 h-10 flex items-center justify-center rounded-full bg-primary bg-opacity-10'
+              >
+                <FiUser size={20} className='text-greenDark' />
+              </Link>
+            </AppTooltip>
+            <AppTooltip content='Giỏ hàng'>
+              <Link
+                to=''
+                className='hover:bg-opacity-30 duration-300 transition-all relative w-10 h-10 flex items-center justify-center rounded-full bg-primary bg-opacity-10'
+              >
+                <FiShoppingCart size={20} className='text-greenDark' />
+                <div className='rounded-full p-[11px] top-[-10%] right-[-10%] absolute w-3 h-3 text-white text-xs bg-red-600 flex items-center justify-center'>
+                  10
+                </div>
+              </Link>
+            </AppTooltip>
           </div>
         </div>
         <div className='lg:hidden block mt-6'>
